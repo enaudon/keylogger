d5eeb2cc0bcf0965a48b2d9a908d3a151e00915e
Enrique Naudon <enaudoncs@gmail.com>
2011-10-31 17:23:10 -0400

    asmhack successfully overwrites the beginning of getpid. the module still crashes after entering the critical section.  probably because of synchronization issues.

--- examples/asmhack/asmhack.c
+++ examples/asmhack/asmhack.c
@@ -1,16 +1,27 @@
+//#include <linux/kernel.h>
+//#include <linux/mm.h>
+//#include <linux/slab.h>
+//#include <asm/uaccess.h>
+//#include <linux/smp_lock.h>
+//#include <asm/semaphore.h>
 #include <linux/init.h>
+#include <linux/mman.h>
 #include <linux/module.h>
+#include <linux/semaphore.h>
 MODULE_LICENSE("DUAL BSD/GPL");
 
 #define JMP_BYTES 7
 
 //sys_getpid address (system-specific)
-long (*getpid)(void) = (void *)0xc1067f20;
+long (*getpid)(void) = (void *)0xC1067F20;
+
+//semaphores
+static struct semaphore gp_sem;
 
 //original and replacement instructions at sys_getpid
 static char orig_instr[JMP_BYTES];
 static char repl_instr[JMP_BYTES] = "\xb8\x00\x00\x00\x00"  //mov eax, 0
-                                  "\xff\xe0";             //jmp eax
+                                    "\xff\xe0";             //jmp eax
 
 /*Copies bytes from the specified source to the specified destination.
  */
@@ -30,19 +41,58 @@ void *_memcpy(void *dest, const void *src, int bytes) {
  *alert.
  */
 asmlinkage long hacked_getpid(void) {
-  printk(KERN_ALERT "All your base are belong to... me?\n");
-  return getpid();
+  long pid;  //sys_getpid return value
+
+  /* Critical Section */
+  printk(KERN_ALERT "Entering critical section... \n");
+  down(&gp_sem);
+  printk(KERN_ALERT "  done!\n");
+
+  //remove write protection
+  write_cr0 (read_cr0 () & (~0x10000));
+  printk(KERN_ALERT "Write protection disabled.\n");
+
+  //restore getpid, call it and overwrite it again
+  _memcpy(getpid, orig_instr, JMP_BYTES);  //restore
+  pid = getpid();                          //call
+  _memcpy(getpid, repl_instr, JMP_BYTES);  //overwrite
+
+  //reenable write protection
+  write_cr0 (read_cr0 () | 0x10000);
+  printk(KERN_ALERT "Write protection enabled.\n");
+
+  printk(KERN_ALERT "Exiting critical section... \n");
+  up(&gp_sem);
+  printk(KERN_ALERT "  done!\n");
+  /* End Critical Section */
+
+  return pid;
 }
 
 /*Module initialization function.
  */
 static int hello_init(void) {
 
+  printk(KERN_ALERT "Hello, world!\n");
+
   //get addr of hacked_getpid into asm framework
-  printk(KERN_ALERT "%X\n", repl_instr[1]);
-  *(long *)&repl_instr[1] = (long)hacked_getpid;
-  printk(KERN_ALERT "%X\n", (long)hacked_getpid);
-  printk(KERN_ALERT "%X\n", repl_instr[1]);
+  *(long *)&repl_instr[1] = (unsigned)hacked_getpid;
+
+  //save and overwrite the bytes at getpid
+  _memcpy(orig_instr, getpid, JMP_BYTES);
+  _memcpy(orig_instr, repl_instr, JMP_BYTES);
+
+
+  //remove write protection
+  write_cr0 (read_cr0 () & (~0x10000));
+  printk(KERN_ALERT "Write protection disabled.\n");
+
+  //THIS LINE CAUSES THE CRASH (i think...)
+  _memcpy(getpid, repl_instr, JMP_BYTES);
+
+  //reenable write protection
+  write_cr0 (read_cr0 () | 0x10000);
+  printk(KERN_ALERT "Write protection enabled.\n");
 
   return 0;
 }
@@ -50,7 +100,11 @@ static int hello_init(void) {
 /*Module clean-up function.
  */
 static void hello_exit(void) {
-  printk(KERN_ALERT "Goobye, world!\n");
+
+  //replace original getpid bytes
+  _memcpy(getpid, orig_instr, JMP_BYTES);
+
+  printk(KERN_ALERT "Goodbye, world!\n");
 }
 
 module_init(hello_init);
