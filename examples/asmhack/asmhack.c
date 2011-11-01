4a33b7254939f7304db73b30e30c80d3fc6de8c1
Enrique Naudon <enaudoncs@gmail.com>
2011-10-31 20:56:13 -0400

    made asthetic changes to asmhack. commented, reorganized and slightly refactored the source code.

--- examples/asmhack/asmhack.c
+++ examples/asmhack/asmhack.c
@@ -1,41 +1,21 @@
-//#include <linux/kernel.h>
-//#include <linux/mm.h>
-//#include <linux/slab.h>
-//#include <asm/uaccess.h>
-//#include <linux/smp_lock.h>
-//#include <asm/semaphore.h>
 #include <linux/init.h>
 #include <linux/mman.h>
 #include <linux/module.h>
-#include <linux/semaphore.h>
 #include <linux/spinlock.h>
-MODULE_LICENSE("DUAL BSD/GPL");
+MODULE_LICENSE("GPL");
 
 #define JMP_BYTES 7
 
-//sys_getpid address (system-specific)
-long (*getpid)(void) = (void *)0xC1067F20;
+void *_memcpy(void *dest, const void *src, int bytes);
+long (*getpid)(void) = (void *)0xC1067F20;  //addr is system-specific
 
-//locks
+//spinlock to protect fancy footwork required to call the original function
 spinlock_t gp_spin = SPIN_LOCK_UNLOCKED;
 
-//original and replacement instructions at sys_getpid
+//original and replacement instructions at getpid
 static char orig_instr[JMP_BYTES];
 static char repl_instr[JMP_BYTES] = "\xb8\x00\x00\x00\x00"  //mov eax, 0
                                     "\xff\xe0";             //jmp eax
-
-/*Copies bytes from the specified source to the specified destination.
- */
-void *_memcpy(void *dest, const void *src, int bytes) {
-  const char *s = src;
-  char *d = dest;
-
-  //swap bytes
-  register int i;
-  for (i = 0; i < bytes; ++i) *d++ = *s++;
-
-  return dest;
-}
 
 /*New sys_getpid function.
  *This is a proof of concept, so there's no need to be mean here--just print an
@@ -49,16 +29,12 @@ asmlinkage long hacked_getpid(void) {
   /* Critical Section */
   spin_lock(&gp_spin);
 
-  //remove write protection
-  write_cr0 (read_cr0 () & (~0x10000));
-
-  //restore getpid, call it and overwrite it again
-  _memcpy(getpid, orig_instr, JMP_BYTES);  //restore
-  pid = getpid();                          //call
-  _memcpy(getpid, repl_instr, JMP_BYTES);  //overwrite
-
-  //reenable write protection
-  write_cr0 (read_cr0 () | 0x10000);
+    //call original getpid function
+    write_cr0 (read_cr0 () & (~0x10000));    //disable write-protection
+    _memcpy(getpid, orig_instr, JMP_BYTES);  //restore original bytes
+    pid = getpid();                          //call original getpid
+    _memcpy(getpid, repl_instr, JMP_BYTES);  //overwrite original bytes
+    write_cr0 (read_cr0 () | 0x10000);       //reenable write-protection
 
   spin_unlock(&gp_spin);
   /* End Critical Section */
@@ -66,46 +42,49 @@ asmlinkage long hacked_getpid(void) {
   return pid;
 }
 
-/*Module initialization function.
+/*Copies bytes from the specified source to the specified destination.
  */
-static int hello_init(void) {
+void *_memcpy(void *dest, const void *src, int bytes) {
+  const char *s = src;
+  char *d = dest;
 
+  //swap memory, one byte at a time
+  register int i;
+  for (i = 0; i < bytes; ++i) *d++ = *s++;
+
+  return dest;
+}
+
+/*Module initialization.
+ */
+static int asm_init(void) {
   printk(KERN_ALERT "Hello, world!\n");
 
   //get addr of hacked_getpid into asm framework
   *(long *)&repl_instr[1] = (unsigned)hacked_getpid;
 
-  //save the bytes at getpid
+  //save original bytes at getpid
   _memcpy(orig_instr, getpid, JMP_BYTES);
 
-
-  //remove write protection
-  write_cr0 (read_cr0 () & (~0x10000));
-
-  //THIS LINE CAUSES THE CRASH (i think...)
-  _memcpy(getpid, repl_instr, JMP_BYTES);
-
-  //reenable write protection
-  write_cr0 (read_cr0 () | 0x10000);
+  //overwrite bytes at get_pid with jump
+  write_cr0 (read_cr0 () & (~0x10000));    //disable write-protection
+  _memcpy(getpid, repl_instr, JMP_BYTES);  //overwrite original bytes
+  write_cr0 (read_cr0 () | 0x10000);       //reenable write-protection
 
   return 0;
 }
 
-/*Module clean-up function.
+/*Module clean-up.
  */
-static void hello_exit(void) {
+static void asm_exit(void) {
 
-  //remove write protection
-  write_cr0 (read_cr0 () & (~0x10000));
-
-  //replace original getpid bytes
-  _memcpy(getpid, orig_instr, JMP_BYTES);
-
-  //reenable write protection
-  write_cr0 (read_cr0 () | 0x10000);
+  //restore original getpid bytes
+  write_cr0 (read_cr0 () & (~0x10000));    //disable write-protection
+  _memcpy(getpid, orig_instr, JMP_BYTES);  //restore original bytes
+  write_cr0 (read_cr0 () | 0x10000);       //reenable write-protection
 
   printk(KERN_ALERT "Goodbye, world!\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(asm_init);
+module_exit(asm_exit);
